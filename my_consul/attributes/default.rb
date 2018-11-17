7290bf61a2700dda9233394f3a3c061f02744e5d
Dac Do <dac.do@fastlane-it.com>
2018-11-18 06:47:35 +1100

    set consul attributes

--- my_consul/attributes/default.rb
+++ my_consul/attributes/default.rb
@@ -1,4 +1,11 @@
-#override['consul']['version'] = '0.9.0'
-override['consul']['version'] = '1.2.2'
+#override['consul']['version'] = '1.2.2'
+default['consul']['version'] = '1.2.2'
+
+default['consul']['server'] = true
+default['consul']['config']['bind_addr'] = node['ipaddress']
+default['consul']['config']['advertise_addr'] = node['ipaddress']
+default['consul']['config']['advertise_addr_wan'] = node['ipaddress']
+
+default['consul']['service']['config_dir'] = '/app/consul/conf.d'
 
 default['my_consul']['server'] = true
